9e3c38af9f0d56fa6184c05156b99e73983a1960
xfokiux xfokiux <xfokiuxxfokiux@192.168.1.5>
2024-07-19 16:46:50 -0500

    leson6

---
+++ src/components/ui/not-found/PageNotFound.tsx
@@ -0,0 +1,13 @@
+
+
+export const PageNotFound = () => {
+  return (
+    <div className="flex flex-col-reverse md:flex-row h-[800px] w-full justify-center items-center align-middle">
+      <div className="text-center px-5 mx-5">
+        <h2>404 page it's not found</h2>
+
+      </div>
+    </div>
+  )
+
+}
